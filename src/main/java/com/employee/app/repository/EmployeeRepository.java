5747639dd79c270eeea4fbb548361887e51f2af3
Imran Ansari <iagro7189@gmail.com>
2023-10-21 03:14:29 +0530

    Adding repository

---
+++ src/main/java/com/employee/app/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.employee.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.employee.app.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+
+}
