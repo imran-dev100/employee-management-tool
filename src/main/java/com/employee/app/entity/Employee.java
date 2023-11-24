f819b68cb9311423c7be9e05a228ebd5c2d67b8d
imran-dev100 <iagro7189@gmail.com>
2023-11-24 21:29:25 +0530

    Adding Default constructor

--- src/main/java/com/employee/app/entity/Employee.java
+++ src/main/java/com/employee/app/entity/Employee.java
@@ -8,9 +8,11 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "employees")
