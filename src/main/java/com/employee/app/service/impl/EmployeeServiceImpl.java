70c1e9bd63f9ede9145ad226a025faf083355e8d
imran-dev100 <iagro7189@gmail.com>
2023-11-24 21:22:11 +0530

    Adding service methods implementation

--- src/main/java/com/employee/app/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/employee/app/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,50 @@
 package com.employee.app.service.impl;
 
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.employee.app.entity.Employee;
+import com.employee.app.exception.EmployeeNotFoundException;
+import com.employee.app.repository.EmployeeRepository;
+import com.employee.app.service.EmployeeService;
+
 @Service
-public class EmployeeServiceImpl {
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public Employee create(final Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee update(final long id, final Employee employee) {
+		final Employee savedEmployee = employeeRepository.findById(id).orElseThrow(EmployeeNotFoundException::new);
+		savedEmployee.setAge(!Objects.equals(employee.getAge(), savedEmployee.getAge()) ? employee.getAge()
+				: savedEmployee.getAge());
+		savedEmployee.setName(!Objects.equals(employee.getName(), savedEmployee.getName()) ? employee.getName()
+				: savedEmployee.getName());
+		return employeeRepository.save(savedEmployee);
+	}
+
+	@Override
+	public Employee fetchById(final long id) {
+		return employeeRepository.findById(id).orElseThrow(EmployeeNotFoundException::new);
+	}
+
+	@Override
+	public List<Employee> fetchAll() {
+		return (List<Employee>) employeeRepository.findAll();
+	}
+
+	@Override
+	public void removeById(final long id) {
+		employeeRepository.deleteById(id);
+	}
 
 }
