bf9167734b8de7e5333589c16ef07876f0cdb9f1
imran-dev100 <iagro7189@gmail.com>
2023-11-24 21:22:01 +0530

    Adding service methods

--- src/main/java/com/employee/app/service/EmployeeService.java
+++ src/main/java/com/employee/app/service/EmployeeService.java
@@ -1,5 +1,18 @@
 package com.employee.app.service;
 
+import java.util.List;
+
+import com.employee.app.entity.Employee;
+
 public interface EmployeeService {
 
+	public Employee create(final Employee employee);
+
+	public Employee update(final long id,final  Employee employee);
+
+	public Employee fetchById(final long id);
+
+	public List<Employee> fetchAll();
+
+	public void removeById(final long id);
 }
