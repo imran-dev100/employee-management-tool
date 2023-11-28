0f3bd6b0b46c527e32c5bb5e35a4e8f2dfa685a2
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2023-11-28 19:18:29 +0530

    Update EmployeeNotFoundException.java

--- src/main/java/com/employee/app/exception/EmployeeNotFoundException.java
+++ src/main/java/com/employee/app/exception/EmployeeNotFoundException.java
@@ -3,7 +3,6 @@ package com.employee.app.exception;
 import jakarta.persistence.EntityNotFoundException;
 
 public class EmployeeNotFoundException extends EntityNotFoundException {
-
 	private static final long serialVersionUID = 335477230763683868L;
 	public EmployeeNotFoundException() {
 		super();
