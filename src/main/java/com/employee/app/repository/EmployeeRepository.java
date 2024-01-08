300eac422680b36cd7f3733f4f0e01411f39c499
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-01-08 14:19:02 +0530

    Update EmployeeRepository.java

--- src/main/java/com/employee/app/repository/EmployeeRepository.java
+++ src/main/java/com/employee/app/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.employee.app.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import com.employee.app.entity.Employee;
 
 @Repository
