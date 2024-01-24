d9e7d6242474ff32830b91a464cd92428aba07c8
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-01-24 21:02:51 +0530

    Update ControllerAdvisor.java

--- src/main/java/com/employee/app/exception/handler/ControllerAdvisor.java
+++ src/main/java/com/employee/app/exception/handler/ControllerAdvisor.java
@@ -3,14 +3,12 @@ package com.employee.app.exception.handler;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import jakarta.persistence.EntityNotFoundException;
 
 @ControllerAdvice
